ab8b2c3714d44eab88b58f4b263f17e3ef0189cc
Simge <33289168+simgehelvaci@users.noreply.github.com>
2026-07-02 10:00:47 +0300

    Initial Commit

---
+++ Starsign/StarsignApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarsignApp.swift
+//  Starsign
+//
+//  Created by Simge Helvaci on 2.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct StarsignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StarsignTests/StarsignTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarsignTests.swift
+//  StarsignTests
+//
+//  Created by Simge Helvaci on 2.07.2026.
+//
+
+import Testing
+@testable import Starsign
+
+struct StarsignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
